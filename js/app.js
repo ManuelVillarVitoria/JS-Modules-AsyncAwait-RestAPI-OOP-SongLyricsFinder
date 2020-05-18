6a0c48b57a74729e531922f652fa7d596e92c862
manu <manu.villar.vitoria@gmail.com>
2020-05-18 17:56:27 +0200

    displaying the query in the html

--- js/app.js
+++ js/app.js
@@ -28,8 +28,22 @@ UI.formularioBuscar.addEventListener('submit', e => {
        const api = new API(artista,cancion);
        api.consularAPI()
             .then(data => {
-                console.log(data);
+                //console.log(data);
+                if(data.respuesta.lyrics) {
+                    //La canción existe
+                    const letra = data.respuesta.lyrics;
+                    UI.divResultado.textContent = letra;
+                } else {
+                    //La canción no existe
+                    UI.divMensajes.innerHTML = 'La canción NO existe, prueba con otra búsqueda';
+                    UI.divMensajes.classList.add('error');
+                    setTimeout(() => {
+                            UI.divMensajes.innerHTML = '';
+                            UI.divMensajes.classList.remove('error');
+                            UI.formularioBuscar.reset();
+                    },3000);    
+                }
             })
    }
-   
+
 });
